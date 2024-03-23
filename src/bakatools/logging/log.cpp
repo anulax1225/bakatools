76800a8c6b8f9da8de67a24d8969b8f5985b6202
anulax1225 <anulax1225@icloud.com>
2024-03-23 01:11:10 +0100

    Modified log class to be able to choose if it should log to file

--- src/bakatools/logging/log.cpp
+++ src/bakatools/logging/log.cpp
@@ -5,11 +5,11 @@ namespace Bk {
 	std::shared_ptr<spdlog::logger> Log::p_core_logger;
 	std::shared_ptr<spdlog::logger> Log::p_app_logger;
 
-	void Log::init(std::string core)
+	void Log::init(std::string core, bool file_log = false)
 	{
 		std::vector<spdlog::sink_ptr> log_sinks;
 		log_sinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
-		log_sinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("bakara.log", true));
+		if (file_log) log_sinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("bakara.log", true));
 
 		log_sinks[0]->set_pattern("%^[%T] %n: %v%$");
 		log_sinks[1]->set_pattern("[%T] [%l] %n: %v");
