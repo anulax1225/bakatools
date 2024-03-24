46284efe285edea9ea3d63550674f2ee9b8c3eba
anulax1225 <anulax1225@icloud.com>
2024-03-24 19:17:03 +0100

    Added include to public interface

--- src/bakatools.h
+++ src/bakatools.h
@@ -4,3 +4,6 @@
 #include <bakatools/logging/log.h> 
 #include <bakatools/logging/assert.h>
 #include <bakatools/data_type/trie.h>
+#include <bakatools/time/time.h>
+#include <bakatools/thread/task_timer.h>
+#include <bakatools/file_system/file_watcher.h>
