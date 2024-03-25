2435291bb86f0ab6858ff4e7eea60ee57e1a1ab0
anulax1225 <anulax1225@icloud.com>
2024-03-25 22:01:22 +0100

    Added condionnal including in the public interface

--- src/bakatools.h
+++ src/bakatools.h
@@ -1,9 +1,19 @@
 #pragma once
 
-#include <bakatools/string/string_tools.h>
-#include <bakatools/logging/log.h> 
-#include <bakatools/logging/assert.h>
-#include <bakatools/data_type/trie.h>
-#include <bakatools/time/time.h>
-#include <bakatools/thread/task_timer.h>
-#include <bakatools/file_system/file_watcher.h>
+#ifdef BKM_STRING
+    #include <bakatools/string/string_tools.h>
+#endif
+#ifdef BKM_BASE
+    #include <bakatools/logging/log.h> 
+    #include <bakatools/logging/assert.h>
+#endif
+#ifdef BKM_TIME_AND_THREADS
+    #include <bakatools/time/time.h>
+    #include <bakatools/thread/task_timer.h>
+#endif
+#ifdef BKM_FILESYSTEM
+    #include <bakatools/file_system/file_watcher.h>
+#endif
+#ifdef BKM_DATASTRUCT
+    #include <bakatools/data_struct/trie.h>
+#endif
