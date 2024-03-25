a474d2ce114e519f44150e56d3b492ab26388ae8
anulax1225 <anulax1225@icloud.com>
2024-03-25 22:57:49 +0100

    Cleaned up public interface

--- src/bakatools.h
+++ src/bakatools.h
@@ -1,19 +1,28 @@
 #pragma once
 
-#ifdef BKM_STRING
-    #include <bakatools/string/string_tools.h>
-#endif
-#ifdef BKM_BASE
+#ifdef BKMOD_BASE
     #include <bakatools/logging/log.h> 
     #include <bakatools/logging/assert.h>
 #endif
-#ifdef BKM_TIME_AND_THREADS
-    #include <bakatools/time/time.h>
+#ifdef BKMOD_STRING
+    #include <bakatools/string/string_tools.h>
+#endif
+#ifdef BKMOD_CONTAINER
+    #include <bakatools/container/data_stream.h>
+    #include <bakatools/container/trie.h>
+#endif
+#ifdef BKMOD_THREAD
     #include <bakatools/thread/task_timer.h>
+    #include <bakatools/thread/task_delayer.h>
+    #define BKMOD_TIME
 #endif
-#ifdef BKM_FILESYSTEM
+#ifdef BKMOD_FILESYSTEM
+    #include <bakatools/file_system/dir_entry.h> 
+    #include <bakatools/file_system/directory.h>
+    #include <bakatools/file_system/file.h>
     #include <bakatools/file_system/file_watcher.h>
+    #define BKMOD_TIME
 #endif
-#ifdef BKM_DATASTRUCT
-    #include <bakatools/data_struct/trie.h>
+#ifdef BKMOD_TIME
+    #include <bakatools/time/time.h>
 #endif
