d7053a3e8170f5718a1665d63d8bc9f61c81a221
anulax1225 <anulax1225@icloud.com>
2024-06-12 09:59:32 +0200

    Removed defines

--- src/bakatools.h
+++ src/bakatools.h
@@ -1,46 +1,19 @@
 #pragma once
 
-#ifdef BKMOD_ALL
-    #define BKMOD_BASE
-    #define BKMOD_JSON
-    #define BKMOD_STRING
-    #define BKMOD_CONTAINER
-    #define BKMOD_THREAD
-    #define BKMOD_FILESYSTEM
-#endif
-#ifdef BKMOD_BASE
-    #include <bakatools/logging/log.h> 
-    #include <bakatools/logging/assert.h>
-#endif
-#ifdef BKMOD_JSON
-    #include <bakatools/json/fs_manager.h>
-    #include <bakatools/json/parser.h>
-    #include <bakatools/json/lexer.h>
-    #include <bakatools/json/node.h>
-    #define BKMOD_CONTAINER
-    #define BKMOD_STRING
-    #define BKMOD_FILESYSTEM
-#endif
-#ifdef BKMOD_STRING
-    #include <bakatools/string/string_tools.h>
-#endif
-#ifdef BKMOD_CONTAINER
-    #include <bakatools/container/data_stream.h>
-    #include <bakatools/container/trie.h>
-#endif
-#ifdef BKMOD_THREAD
-    #include <bakatools/thread/task_timer.h>
-    #include <bakatools/thread/task_delayer.h>
-    #define BKMOD_TIME
-#endif
-#ifdef BKMOD_FILESYSTEM
-    #include <bakatools/file_system/dir_entry.h> 
-    #include <bakatools/file_system/directory.h>
-    #include <bakatools/file_system/file.h>
-    #include <bakatools/file_system/file_watcher.h>
-    #define BKMOD_TIME
-#endif
-#ifdef BKMOD_TIME
-    #include <bakatools/time/time_span.h>
-    #include <bakatools/time/time_point.h>
-#endif
+#include <bakatools/logging/log.h> 
+#include <bakatools/logging/assert.h>
+#include <bakatools/json/fs_manager.h>
+#include <bakatools/json/parser.h>
+#include <bakatools/json/lexer.h>
+#include <bakatools/json/node.h>
+#include <bakatools/string/string_tools.h>
+#include <bakatools/container/data_stream.h>
+#include <bakatools/container/trie.h>
+#include <bakatools/thread/task_timer.h>
+#include <bakatools/thread/task_delayer.h>
+#include <bakatools/file_system/dir_entry.h> 
+#include <bakatools/file_system/directory.h>
+#include <bakatools/file_system/file.h>
+#include <bakatools/file_system/file_watcher.h>
+#include <bakatools/time/time_span.h>
+#include <bakatools/time/time_point.h>
