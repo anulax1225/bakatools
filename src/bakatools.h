67935579089f30e41acf5a3a56b280c96e9d681c
anulax1225 <anulax1225@icloud.com>
2024-06-18 23:16:55 +0200

    Cleaned up

--- src/bakatools.h
+++ src/bakatools.h
@@ -1,26 +1,28 @@
 #pragma once
 
-#include <bakatools/logging/log.h> 
-#include <bakatools/logging/assert.h>
+#include "bakatools/base.h"
 
-#include <bakatools/json/parser.h>
-#include <bakatools/json/lexer.h>
-#include <bakatools/json/node.h>
-#include <bakatools/json/fs_manager.h>
-#include <bakatools/json/tools.h>
+#include "bakatools/logging/log.h" 
+#include "bakatools/logging/assert.h"
 
-#include <bakatools/string/string_tools.h>
+#include "bakatools/json/parser.h"
+#include "bakatools/json/lexer.h"
+#include "bakatools/json/node.h"
+#include "bakatools/json/fs_manager.h"
+#include "bakatools/json/tools.h"
 
-#include <bakatools/container/data_stream.h>
-#include <bakatools/container/trie.h>
+#include "bakatools/string/string_tools.h"
 
-#include <bakatools/thread/task_timer.h>
-#include <bakatools/thread/task_delayer.h>
+#include "bakatools/container/data_stream.h"
+#include "bakatools/container/trie.h"
 
-#include <bakatools/file_system/dir_entry.h> 
-#include <bakatools/file_system/directory.h>
-#include <bakatools/file_system/file.h>
-#include <bakatools/file_system/file_watcher.h>
+#include "bakatools/thread/task_timer.h"
+#include "bakatools/thread/task_delayer.h"
 
-#include <bakatools/time/time_span.h>
-#include <bakatools/time/time_point.h>
+#include "bakatools/file_system/dir_entry.h" 
+#include "bakatools/file_system/directory.h"
+#include "bakatools/file_system/file.h"
+#include "bakatools/file_system/file_watcher.h"
+
+#include "bakatools/time/time_span.h"
+#include "bakatools/time/time_point.h"
